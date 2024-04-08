19aaba786cf43aee8b3a72c77f3544e1dc984250
Yedidia AGNIMO <ayedidyas@gmail.com>
2024-04-08 10:29:00 +0200

    chore: initialize project structure using Cookiecutter
    * Move original notebooks to notebooks/original_repo_files
    * Move data/movies_tmdb.pkl to data/original_repo_files

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Leverage data on movie to get valuable insights',
+    author='AGNIMO Yedidia, AYMAN Limane, DIONE Oumar',
+    license='MIT',
+)
