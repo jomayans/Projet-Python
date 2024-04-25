b64b88d421c9511bb016554a1cb41253246f8baa
Yedidia AGNIMO <100226662+Yedson54@users.noreply.github.com>
2024-04-25 11:43:56 +0200

    Project structure (#4)
    * docs: Add a requirements and new .gitignore entries for data
    
    * style: improve scripts formatting.

--- src/data/utils.py
+++ src/data/utils.py
@@ -1,3 +1,5 @@
+"""Utility functions for data cleaning.
+"""
 ##############################################################################
 ############## DEFINE FUNCTIONS FOR DATA CLEANING ############################
 ##############################################################################
@@ -9,35 +11,35 @@
 
 
 def extract_only_names(cell):
-    """Select the element whose attribute is name in a list of json documents. 
-    
+    """Select the element whose attribute is name in a list of json documents.
+
     Parameters
     ----------
         cell: list, list of json documents,
-    
+
     Return
     ------
         list: list of names extarcted.
-        
+
     Example
     -------
     cell = [
-        {"id": 15, "name": "Drama"}, 
-        {"id": 30, "name": "Thriller"}, 
+        {"id": 15, "name": "Drama"},
+        {"id": 30, "name": "Thriller"},
         {"id": 45, "name": "Adventure"}
     ]
     cellnames = extract_only_names(cell)
     print(cellnames)
     >>> ['Drama', 'Thriller', 'Adventure']
-    
-    # Application to column `genres` 
+
+    # Application to column `genres`
     movies_df["genres"] = movies_df["genres"].apply(extract_only_names)
     """
-    
+
     if isinstance(cell, list):
-        variable = [] 
+        variable = []
         for element in cell:
-            variable.append(element["name"])  
+            variable.append(element["name"])
     return variable
 
 
@@ -46,18 +48,20 @@ def extract_only_names(cell):
 #######################################
 
 
-def select_first_element(x):
+def select_first_element(row):
     """Return first element for a list element. Use to process variables with
     list elements.
-    
-    Parameters
+
+    Parametersgit 
     ----------
-        x: list, list 
-    
+        row: list, list
+
     Return
     ------
         string: first element of the list.
     """
-    
-    if isinstance(x, list):
-        return str(x[0])
+
+    if isinstance(row, list):
+        return str(row[0])
+    else:
+        return None
