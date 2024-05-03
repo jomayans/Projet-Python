bbc8421c374ab60d9c790a70babd622c45d1d5a5
odione <oumar.dione@ensae.fr>
2024-05-03 07:53:51 +0000

    Ajout mise à jour du charger de données pour les récéupérer depuis l bucket s3 de AYMAN

--- src/data/Import_data_by_PARK.py
+++ src/data/Import_data_by_PARK.py
@@ -1,15 +1,37 @@
 import pandas as pd
 import IPython.display as display
 
-def load_data(in_path, name, n_display=1, show_info=False, nrows=None):
-    df = pd.read_parquet(in_path, nrows=nrows)
-    print(f"{name}: shape is {df.shape}")
-    df = df.rename(columns={'keywords': 'Keywords'})
+import boto3
+import pyarrow.parquet as pq
+# Exemple d'utilisation
+minio_url = 'URL_VERS_MINIO'
+access_key = 'VOTRE_ACCESS_KEY'
+secret_key = 'VOTRE_SECRET_KEY'
+bucket_name = 'NOM_DU_BUCKET'
+object_key = 'CHEMIN_VERS_LE_FICHIER_DANS_LE_BUCKET'
 
-    if show_info:
-        print(df.info())
-    
-    if n_display > 0:
-        display.display(df.head(n_display))
+def read_parquet_from_minio(minio_url, access_key, secret_key, bucket_name, object_key):
+    # Connexion à MinIO
+    s3 = boto3.client('s3', endpoint_url=minio_url, aws_access_key_id=access_key, aws_secret_access_key=secret_key)
+
+    try:
+        # Téléchargement du fichier Parquet depuis le bucket MinIO
+        response = s3.get_object(Bucket=bucket_name, Key=object_key)
+        # Lecture du fichier Parquet
+        table = pq.read_table(response['Body'])
+        return table
+    except Exception as e:
+        print(f"Une erreur s'est produite lors de la lecture du fichier Parquet : {e}")
+        return None
+
+
+
+parquet_table = read_parquet_from_minio(minio_url, access_key, secret_key, bucket_name, object_key)
+if parquet_table is not None:
+    # Affichage des premières lignes du tableau Parquet
+    print(parquet_table.to_pandas().head())
+
+
+
         
     return df
